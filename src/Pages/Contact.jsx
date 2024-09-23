9f5aef46690160cfc1e0dfb7e298da29561b7962
Subodh <subodhpokrel10@gmail.com>
2024-09-23 17:20:01 +0545

    initial commit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,11 @@
 const Contact = () => {
   return (
-    <section className=" h-screen ">
-      <p>Contact Page</p>
+    <section
+      className="h-screen flex flex-col justify-center items-center text-center p-4 bg-cover bg-center text-white font-inter font-bold"
+      style={{ backgroundImage: "url('assets/Initial.png')" }}
+    >
+      <div className="text-6xl md:text-8xl font-bold mb-4 font-bebas">WIP</div>
+      <p className="text-xl md:text-3xl mb-8 font-dancing">Due to change.</p>
     </section>
   );
 };
