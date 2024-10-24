09f686cb019f928967afa91c0996092832471ae3
Subodh <subodhpokrel10@gmail.com>
2024-10-24 19:34:27 +0545

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
-  <Analytics>
+  <>
     <App />
-  </Analytics>
+    <Analytics />
+  </>
 );
